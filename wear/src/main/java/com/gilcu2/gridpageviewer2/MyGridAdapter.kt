d6fccccb66eafed741cce4211190f84a82766874
gilcu2 <gilcu2@gmail.com>
2017-02-28 07:34:21 +0100

    beginning two views

--- wear/src/main/java/com/gilcu2/gridpageviewer2/MyGridAdapter.kt
+++ wear/src/main/java/com/gilcu2/gridpageviewer2/MyGridAdapter.kt
@@ -6,10 +6,18 @@ package com.gilcu2.gridpageviewer2
 
 import android.content.Context
 import android.graphics.Color
+import android.support.v4.content.ContextCompat
 import android.support.wearable.view.GridPagerAdapter
+import android.util.TypedValue
+import android.view.Gravity
+import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.TextView
+import com.learnandroidwear.simplegridview.R
 
 
 class MyGridAdapter(internal var mContext: Context, internal var mImages: IntArray) : GridPagerAdapter() {
@@ -19,8 +27,7 @@ class MyGridAdapter(internal var mContext: Context, internal var mImages: IntArr
     }
 
     override fun getColumnCount(i: Int): Int {
-
-        if (i == 0) return 3 else return 2
+        return 2
     }
 
     //---Go to current column when scrolling up or down (instead of default column 0)---
@@ -30,18 +37,10 @@ class MyGridAdapter(internal var mContext: Context, internal var mImages: IntArr
 
     //---Return our car image based on the provided row and column---
     override fun instantiateItem(viewGroup: ViewGroup, row: Int, col: Int): Any {
-        // Trick to use single dimensional array as multidimensional array.
-        var imageToDisplay = 0
-        when (row) {
-            0 -> imageToDisplay = col     // results are 0, 1, 2
-            1 -> imageToDisplay = 3 + col     // results are 3, 4, 5
-        }
-        val imageView: ImageView
-        imageView = ImageView(mContext)
-        imageView.setImageResource(mImages[imageToDisplay])
-        imageView.setBackgroundColor(Color.parseColor("#26ce61"))
-        viewGroup.addView(imageView)
-        return imageView
+        if (col == 0)
+            return createPage1(viewGroup)
+        else
+            return createPage2(viewGroup)
     }
 
     override fun destroyItem(viewGroup: ViewGroup, i: Int, i2: Int, o: Any) {
@@ -50,5 +49,32 @@ class MyGridAdapter(internal var mContext: Context, internal var mImages: IntArr
 
     override fun isViewFromObject(view: View, o: Any): Boolean {
         return view == o
+    }
+
+    fun createPage1(viewGroup: ViewGroup): Any {
+
+        val frame = FrameLayout(mContext)
+
+        val text = TextView(mContext)
+        text.text = "1"
+        text.gravity = Gravity.CENTER or Gravity.TOP
+        text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 80f)
+        frame.addView(text)
+
+        val button = ImageButton(mContext)
+        button.setImageDrawable(ContextCompat.getDrawable(mContext, R.drawable.ic_brightness_4_black_24dp))
+        val size = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 40f, getResources().getDisplayMetrics())
+        button.wi
+
+
+        viewGroup.addView(frame)
+        return frame
+    }
+
+    fun createPage2(viewGroup: ViewGroup): Any {
+        val text = TextView(mContext)
+        text.text = "2"
+        viewGroup.addView(text)
+        return text
     }
 }
